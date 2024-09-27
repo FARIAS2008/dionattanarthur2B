38daaad95aa57a536e980c645d2698dc4ed4b8a8
FARIAS2008 <164515267+FARIAS2008@users.noreply.github.com>
2024-09-27 13:59:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Messi',
+    'Quantas champions tem o messi ?',
+    'Tem 4 champions '
+)
+
+criaCartao(
+    'CR7',
+    'Quantos gols tem CR7?',
+    'Tem 900 gols '
+)
+
+criaCartao(
+    'Campeonato Paulista ',
+    'Quem ganhou mais paulistão?',
+    'Corinhitians'
+)
+
+criaCartao(
+    'Campeonatos ',
+    'Qual  time que ganhou mais champions e qual ganhou mais libertadores ?',
+    'Real madrid ganhou mais champions e o  Indepiendiente ganhou mais libertadores'
+)
